22316c8e471434a6deb4b2be56d73ee560de6ec9
Abdelilah Khossan <49311230+akhossanX@users.noreply.github.com>
2024-04-08 22:23:32 +0000

    Fix: Deprecated ugettext_lazy for django4.x
    Replace deprecated ugettext_lazy with gettext_lazy

--- testing/testing/app/models.py
+++ testing/testing/app/models.py
@@ -16,7 +16,7 @@
 
 
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 COLOR_CHOICES = (('default', _('Default')),
                  ('primary', _('Primary')),
